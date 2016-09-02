4b35b1aced8bf841fc006864dcee23991276b603
renhengli518 <372594984@qq.com>
2016-09-02 17:33:02 +0800

    delete

--- src/main/java/com/hengpeng/itfintracker/dao/MediaMapper.java
+++
@@ -1,9 +0,0 @@
-package com.hengpeng.itfintracker.dao;
-
-import com.hengpeng.itfintracker.entity.Media;
-
-public interface MediaMapper {
-    int insert(Media record);
-
-    int insertSelective(Media record);
-}
