62eccb5e487e97bccd3b0d28aed6e49d92a280ad
renhengli518 <372594984@qq.com>
2016-09-07 16:35:37 +0800

    add try catch

--- src/main/java/com/hengpeng/itfintracker/controller/UserBehaviorRecordController.java
+++ src/main/java/com/hengpeng/itfintracker/controller/UserBehaviorRecordController.java
@@ -53,34 +53,38 @@ public class UserBehaviorRecordController {
 	@RequestMapping(value = "list", method = RequestMethod.GET)
 	@ResponseBody
 	public Page getUserBehaviorRecordPageList(HttpServletRequest request, String date_start, String date_end, String viewType, Page page) {
-		logger.debug("查询用户行为分页信息开始");
-		Object o = RedisTemplateUtils
-				.get("userBehavior.list." + date_start + "_" + date_end + "_" + viewType + "_" + page.getStart() + "_" + page.getEnd());
-		// List<?> list =
-		// RedisTemplateUtils.lget("userBehavior_1.list."+date_start+"_"+date_end+"_"+viewType+"_"+page.getStart()+"_"+page.getEnd());
-		if (o != null) {
-			// page.setData((List<UserBehaviorRecord>) o);
-			page = (Page) o;
-		} else {
-			Map<String, Object> map = new HashMap<String, Object>();
-			if (StringUtils.isNotEmpty(date_start)) {
-				map.put("date_start", date_start);
+		try {
+			logger.info("查询用户行为分页信息开始");
+			Object o = RedisTemplateUtils
+					.get("userBehavior.list." + date_start + "_" + date_end + "_" + viewType + "_" + page.getStart() + "_" + page.getEnd());
+			// List<?> list =
+			// RedisTemplateUtils.lget("userBehavior_1.list."+date_start+"_"+date_end+"_"+viewType+"_"+page.getStart()+"_"+page.getEnd());
+			if (o != null) {
+				page = (Page) o;
+			} else {
+				Map<String, Object> map = new HashMap<String, Object>();
+				if (StringUtils.isNotEmpty(date_start)) {
+					map.put("date_start", date_start);
+				}
+				if (StringUtils.isNotEmpty(viewType)) {
+					map.put("viewType", viewType);
+				}
+				if (StringUtils.isNotEmpty(date_end)) {
+					map.put("date_end", date_end);
+				}
+				page.setMap(map);
+				page = pageViewService.getUserBehaviorRecordPageList(page);
+				RedisTemplateUtils.set("userBehavior.list." + date_start + "_" + date_end + "_" + viewType + "_" + page.getStart() + "_" + page.getEnd(),
+						page, redisCacheTimeout);
+				// RedisTemplateUtils.lpush("userBehavior_1.list."+date_start+"_"+date_end+"_"+viewType+"_"+page.getStart()+"_"+page.getEnd(),
+				// page.getData(),redisCacheTimeout);
 			}
-			if (StringUtils.isNotEmpty(viewType)) {
-				map.put("viewType", viewType);
-			}
-			if (StringUtils.isNotEmpty(date_end)) {
-				map.put("date_end", date_end);
-			}
-			page.setMap(map);
-			page = pageViewService.getUserBehaviorRecordPageList(page);
-			RedisTemplateUtils.set("userBehavior.list." + date_start + "_" + date_end + "_" + viewType + "_" + page.getStart() + "_" + page.getEnd(),
-					page, redisCacheTimeout);
-			// RedisTemplateUtils.lpush("userBehavior_1.list."+date_start+"_"+date_end+"_"+viewType+"_"+page.getStart()+"_"+page.getEnd(),
-			// page.getData(),redisCacheTimeout);
+			logger.info("查询用户行为分页信息结束");
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.info(e.getMessage()) ;
 		}
-		logger.debug("查询用户行为分页信息结束");
-		return page;
+		return page; 
 	}
 
 	@SuppressWarnings("unchecked")
@@ -88,36 +92,41 @@ public class UserBehaviorRecordController {
 	@ResponseBody
 	public ReturnResultUtil exportUserBehaviorRecordList(HttpServletRequest request, HttpServletResponse response, String date_start, String date_end,
 			String viewType) throws Exception {
-		logger.debug("导出用户行为列表开始");
-		List<UserBehaviorRecord> list = new ArrayList<UserBehaviorRecord>();
-		Object o = RedisTemplateUtils.get("userBehavior.list.export." + date_start + date_end + viewType);
-		if (o != null) {
-			// page.setData((List<UserBehaviorRecord>) o);
-			list = (List<UserBehaviorRecord>) o;
-		} else {
-
-			Map<String, Object> map = new HashMap<String, Object>();
-			if (StringUtils.isNotEmpty(date_start)) {
-				map.put("date_start", date_start);
+		try {
+			logger.info("导出用户行为列表开始");
+			List<UserBehaviorRecord> list = new ArrayList<UserBehaviorRecord>();
+			Object o = RedisTemplateUtils.get("userBehavior.list.export." + date_start + date_end + viewType);
+			if (o != null) {
+				list = (List<UserBehaviorRecord>) o;
+			} else {
+				
+				Map<String, Object> map = new HashMap<String, Object>();
+				if (StringUtils.isNotEmpty(date_start)) {
+					map.put("date_start", date_start);
+				}
+				if (StringUtils.isNotEmpty(viewType)) {
+					map.put("viewType", viewType);
+				}
+				if (StringUtils.isNotEmpty(date_end)) {
+					map.put("date_end", date_end);
+				}
+				list = pageViewService.getUserBehaviorRecordList(map);
+				RedisTemplateUtils.set("userBehavior.list.export." + date_start + date_end + viewType, list, 30l);
 			}
-			if (StringUtils.isNotEmpty(viewType)) {
-				map.put("viewType", viewType);
+			if (CollectionUtils.isNotEmpty(list)) {
+				String filePath = UserBehaviorExcelUtils.exportUserBehaviorRecordList(list, exportPath);
+				// 自动下载文件开始
+				// request.getRequestDispatcher("/downLoadFileServlet?url="+filePath).forward(request,
+				// response);
+				logger.info("导出用户行为列表结束");
+				return new ReturnResultUtil(ReturnConstant.RETURN_VALUE_00, "导出成功", filePath);
+			} else {
+				return new ReturnResultUtil(ReturnConstant.RETURN_VALUE_01, "没有需要导出的列表");
 			}
-			if (StringUtils.isNotEmpty(date_end)) {
-				map.put("date_end", date_end);
-			}
-			list = pageViewService.getUserBehaviorRecordList(map);
-			RedisTemplateUtils.set("userBehavior.list.export." + date_start + date_end + viewType, list, 30l);
-		}
-		if (CollectionUtils.isNotEmpty(list)) {
-			String filePath = UserBehaviorExcelUtils.exportUserBehaviorRecordList(list, exportPath);
-			// 自动下载文件开始
-			// request.getRequestDispatcher("/downLoadFileServlet?url="+filePath).forward(request,
-			// response);
-			logger.debug("导出用户行为列表结束");
-			return new ReturnResultUtil(ReturnConstant.RETURN_VALUE_00, "导出成功", filePath);
-		} else {
-			return new ReturnResultUtil(ReturnConstant.RETURN_VALUE_01, "无需下载");
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.info(e.getMessage());
+			return new ReturnResultUtil(ReturnConstant.RETURN_VALUE_01, "后台异常");		
 		}
 	}
 }
